9c76cf36520b4e399868fdbd9b273e157f644e0c
AyeshaIdrees123 <ayesha.idrees@manafatech.com>
2024-12-09 15:22:12 +0500

    edit in main

--- lib/screens/search_notes_screen.dart
+++ lib/screens/search_notes_screen.dart
@@ -41,7 +41,7 @@ class _SearchNotesScreenState extends State<SearchNotesScreen> {
       final note = noteData.data();
       notes.add(note);
       print("objects");
-      print("hello");
+      print("helloooo");
     }
   }
 
